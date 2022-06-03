7b6693a09e023739581326e1ce199503ff8145ae
Sichan Kim <qwer2982@naver.com>
2022-06-03 21:40:51 +0900

    main

--- ordernew.php
+++ ordernew.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+    <link rel="shortcut icon" href="D.jpg" type="image/x-icon" />
     <link href="bootstrap.min.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
     integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
